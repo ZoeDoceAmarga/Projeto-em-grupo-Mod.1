79f9c8d785be8ea20a0b3f66622165603549a26f
Juevan <86933408+Juevan@users.noreply.github.com>
2022-10-14 00:12:48 -0300

    Validação do Formulário.
    Adicionei a verificação de validade de preenchimento do formulário de contato.

--- js/script.js
+++ js/script.js
@@ -42,3 +42,47 @@ function menuRes(){
         buttonBurguer.classList.add('active');;
     }
 }
+// Verificação Formulário
+/*Criando as Variaveis*/
+const form = document.getElementById('form');
+const campos = document.querySelectorAll('.input-check');
+const spans = document.querySelectorAll('.span-check');
+const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+/*Função de erro*/
+function setError(index){
+  campos[index].style.border = '2px solid #db3236';
+  spans[index].style.display = 'block';
+}
+/*Função de remoção do erro*/
+function removeError(index){
+  campos[index].style.border = '2px solid #3cba54';
+  spans[index].style.display = 'none';
+}
+/*Função de validação do nome*/
+function nameValidation(){
+  if (campos[0].value.length < 3){
+    setError(0);
+  }
+  else{
+    removeError(0);
+  }
+}
+/*Função de validação do email*/
+function emailValidation(){
+  if(!emailRegex.test(campos[1].value)){
+    setError(1);
+  }
+  else{
+    removeError(1);
+  }
+}
+/*Função de validação do tel*/
+function telValidation(){
+  if(campos[2].value.length == 11){
+    removeError(2);
+  }
+  else{
+    setError(2);
+  }
+}
