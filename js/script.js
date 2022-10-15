c078f08230d5e197966687b9404bea723e435f4b
Juevan <86933408+Juevan@users.noreply.github.com>
2022-10-15 00:05:32 -0300

    Correção de Bug.
    Corrigidor bug no Pop up e contato

--- js/script.js
+++ js/script.js
@@ -1,33 +1,8 @@
-
-function iniciaModal(modalID) {
-    const modal = document.getElementById(modalID);
-    if(modal){
-        modal.classList.add("mostrar"); //adicionar a classe modal a classe mostrar onde está configurada para aparecer o modal, com um display flex
-
-
-        // ao clicar no "x" ou fora do pop up, irá fechar, removendo a classe mostrar da classe modal
-        modal.addEventListener('click', (evento) =>{
-            if(evento.target.id == modalID || evento.target.className=='fechar'){
-                modal.classList.remove('mostrar');
-                localStorage.fechaModal=modalID
-               }
-            });
-    }
-  }
-
-//ao clicar na div irá aparecer o popup
-//   const logo = document.querySelector(".logo");
-//   logo.addEventListener('click',() => 
-//     iniciaModal("modalPromo"));
-
-
-// pop-up vai aparecer ao scrolar o site
-  document.addEventListener('scroll',() => {
-    if(window.pageYOffset>400){
-      iniciaModal("modalPromo")
-    }
-  })
-
+// Fechar o Popup
+const popup = document.getElementById('modalPromo');
+function closePop(){
+  popup.style.display = 'none';
+}
 
 // Menu Responsivo
 // quando o media query estiver ativado e o botão for clicado, o menu irá aparecer porque o '.menu ul.open' está configurada com altura de 250px. E se o botão for clicado de novo, o menu irá desaparecer porque o '.menu ul" está com altura de 0px.
